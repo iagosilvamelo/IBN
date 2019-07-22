2024bbfba81a804797d450cda30d082292e32386
Iago Melo <iagosilvamelo@gmail.com>
2019-07-22 19:23:00 -0300

    Alterado icone de contato pata w-app e texto do SingUp

--- resources/views/sections/contato.blade.php
+++ resources/views/sections/contato.blade.php
@@ -31,7 +31,7 @@
       <div class="col-md-4 mb-3 mb-md-0">
         <div class="card py-4 h-100">
           <div class="card-body text-center">
-            <i class="fas fa-mobile-alt text-primary mb-2"></i>
+            <i class="fab fa-whatsapp text-primary mb-2"></i>
             <h4 class="text-uppercase m-0">Phone</h4>
             <hr class="my-4">
             <div class="small text-black-50">+55 (51) 9902-8832</div>
